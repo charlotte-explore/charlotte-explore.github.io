e1de7deb4eced3bd92fae3bada207b454d682a5e
Hunter Stingel <hunterstingel@gmail.com>
2022-10-12 23:24:50 -0400

    logger test commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const btn = document.querySelector('button');
 
 function logger(){
-    console.log("Button was Clicked")
+    console.log("Button was Clicked");
 }
 
 
 btn.onclick = () => {
     alert('Hello World');
+    logger();
 }
